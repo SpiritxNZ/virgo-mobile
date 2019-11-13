21315b3425ef45b502f9c2272d85a62ed9081748
tee1365 <31180529+tee1365@users.noreply.github.com>
2019-11-13 15:45:15 +1300

    update

--- components/StyledTitle.js
+++ components/StyledTitle.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import { View, StyleSheet,Text } from 'react-native';
-
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
 
 export default function StyledTitle() {
-    return (
-        <View>
-            <Text style={styles.label}>Virgo's Resurrection</Text>
-            <View style={styles.line}></View>
-        </View>
-    )
+  return (
+    <View>
+      <Text style={styles.label}>Virgo's Resurrection</Text>
+      <View style={styles.line}></View>
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    label:{
-      fontSize: 22,
-      textAlign: 'center',
-      fontWeight:'300'
-    },
-    line:{
-      marginTop: 15,
-      marginLeft:44,
-      borderBottomColor:'black',
-      borderBottomWidth: 2,
-      width:'80%',
-    }
-    });
+  label: {
+    fontSize: 22,
+    textAlign: "center",
+    fontWeight: "300"
+  },
+  line: {
+    marginTop: 15,
+    marginLeft: 44,
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+    width: "80%"
+  }
+});
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,76 +1,72 @@
-import React, { Component } from 'react';
-import { View, StyleSheet,Text} from 'react-native';
-import {Radio, RadioGroup} from 'react-native-ui-kitten'
-import StyledTitle from '../components/StyledTitle';
-import {connect} from 'react-redux'
+import React, { Component } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { Radio, RadioGroup } from "react-native-ui-kitten";
+import StyledTitle from "../components/StyledTitle";
+import { connect } from "react-redux";
 
-class  LinksScreen extends Component {
+class LinksScreen extends Component {
   state = {
-    selectedIndex: 0,
+    selectedIndex: 0
   };
 
-  onChange = (selectedIndex) => {
+  static navigationOptions = {
+    header: null
+  };
+
+  onChange = selectedIndex => {
     this.setState({ selectedIndex });
   };
 
-  render(){
-     return (
-    <View>
-      <StyledTitle/>
+  render() {
+    return (
+      <View>
+        <StyledTitle />
 
-      <View >
-         <Text style={styles.lancho} >CHOOSE LANGUAGE</Text>
+        <View>
+          <Text style={styles.lancho}>CHOOSE LANGUAGE</Text>
+        </View>
+
+        <RadioGroup
+          selectedIndex={this.state.selectedIndex}
+          onChange={this.onChange}
+        >
+          <Radio style={styles.radio} text="ENGLISH" />
+          <Radio style={styles.radio} text="中文" />
+        </RadioGroup>
       </View>
-
-      <RadioGroup
-        selectedIndex={this.state.selectedIndex}
-        onChange={this.onChange}>
-        <Radio style={styles.radio} text='ENGLISH' />
-        <Radio style={styles.radio} text='中文' />
-      </RadioGroup>
-
-    </View>
-  );
+    );
   }
- 
 }
-
-
 
 const styles = StyleSheet.create({
-label:{
-  fontSize: 22,
-  textAlign: 'center',
-  fontWeight:'300'
-},
-line:{
-  marginTop: 15,
-  marginLeft:44,
-  borderBottomColor:'black',
-  borderBottomWidth: 2,
-  width:'80%',
-},
-lancho:{
-  marginTop: 15,
-  marginLeft:44,
-  fontSize:18,
-  fontWeight: '100',
-},
-radio: {
-  marginLeft:44,
-  marginVertical: 4,
-},
+  label: {
+    fontSize: 22,
+    textAlign: "center",
+    fontWeight: "300"
+  },
+  line: {
+    marginTop: 15,
+    marginLeft: 44,
+    borderBottomColor: "black",
+    borderBottomWidth: 2,
+    width: "80%"
+  },
+  lancho: {
+    marginTop: 15,
+    marginLeft: 44,
+    fontSize: 18,
+    fontWeight: "100"
+  },
+  radio: {
+    marginLeft: 44,
+    marginVertical: 4
+  }
 });
 
-const mapStateToProps = () =>{
-  return {
-  
-  }
-}
+const mapStateToProps = () => {
+  return {};
+};
 
-const mapDispatchToProps = () =>{
-
-}
-
+const mapDispatchToProps = () => {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(LinksScreen);
